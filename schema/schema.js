f26c84e3987744db17d1c0917f021d5ca0a062d6
Ahnming Loke <lokeahnming@gmail.com>
2020-10-23 19:02:11 -0400

    feat: hardcoded list of users, created RootQuery and exported schema to server

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,25 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
+
+/* in absence of a database in this example, using hardcoded list of users */
+const users = [
+  { id: '23',
+    firstName: 'Idris',
+    age: '48'
+  },
+  {
+    id: '27',
+    firstName: 'Ruth',
+    age: '38'
+  }
+]
 
 /* dictates properties GQL object needs */
 const UserType = new GraphQLObjectType({
@@ -13,4 +29,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString},
     age: {type: GraphQLInt}
   }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+       type: UserType,
+       args: {  id: { type: GraphQLString }},
+       resolve(parentValue, args) {
+         return _.find(users, { id: args.id });
+       }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
